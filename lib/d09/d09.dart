755d912f8816f84c7b4ccb742ee4f1d6d4f24e69
rgsone <rgs@rgsone.com>
2022-12-11 13:11:30 +0100

    refactor d09

--- lib/d09/d09.dart
+++ lib/d09/d09.dart
@@ -14,53 +14,21 @@ void run() {
 ////////////////////////////////////////////////////////////////////
 
 void resolvePuzzle01(List<String> input) {
-  var head = Point(0, 0);
-  var tail = Point(0, 0);
-  var tailVisited = <Point<int>>{Point(0, 0)};
-
-  Point<int> getDirectionPoint(String direction) {
-    if (direction == 'R')
-      return Point(1, 0);
-    else if (direction == 'D')
-      return Point(0, -1);
-    else if (direction == 'L')
-      return Point(-1, 0);
-    else if (direction == 'U') return Point(0, 1);
-
-    return Point(0, 0);
-  }
-
-  void updateRope(Point<int> dir) {
-    head += dir;
-
-    if ((head.x - tail.x).abs() > 1 || (head.y - tail.y).abs() > 1) {
-      var xDir =
-          head.x == tail.x ? 0 : (head.x - tail.x) ~/ (head.x - tail.x).abs();
-      var yDir =
-          head.y == tail.y ? 0 : (head.y - tail.y) ~/ (head.y - tail.y).abs();
-      tail += Point(xDir, yDir);
-    }
-  }
-
-  input.forEach((line) {
-    var instruction = line.split(' ');
-    var steps = int.parse(instruction.last);
-    var dirPoint = getDirectionPoint(instruction.first);
-
-    for (int i = 0; i < steps; i++) {
-      updateRope(dirPoint);
-      tailVisited.add(Point(tail.x, tail.y));
-    }
-  });
-
-  print('part 01 : ${tailVisited.length}');
+  print('part 01 : ${simRope(input, 2)}');
 }
 
 ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
 
 void resolvePuzzle02(List<String> input) {
-  var knots = List<Point<int>>.generate(10, (index) => Point(0, 0));
+  print('part 02 : ${simRope(input, 10)}');
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+int simRope(List<String> input, int knotsCount) {
+  var knots = List<Point<int>>.generate(knotsCount, (index) => Point(0, 0));
   var tailVisited = <Point<int>>{Point(0, 0)};
 
   Point<int> getDirectionPoint(String direction) {
@@ -97,5 +65,5 @@ void resolvePuzzle02(List<String> input) {
     }
   });
 
-  print('part 02 : ${tailVisited.length}');
+  return tailVisited.length;
 }
