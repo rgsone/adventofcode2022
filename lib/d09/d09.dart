f3f9cfe619f0c6011d5b1e32a8c95b9cffc806c1
rgsone <rgs@rgsone.com>
2022-12-11 13:02:56 +0100

    add d09 part 2

--- lib/d09/d09.dart
+++ lib/d09/d09.dart
@@ -60,6 +60,42 @@ void resolvePuzzle01(List<String> input) {
 ////////////////////////////////////////////////////////////////////
 
 void resolvePuzzle02(List<String> input) {
-  int result = 0;
-  print('part 02 : $result');
+  var knots = List<Point<int>>.generate(10, (index) => Point(0, 0));
+  var tailVisited = <Point<int>>{Point(0, 0)};
+
+  Point<int> getDirectionPoint(String direction) {
+    var x = direction == 'R' ? 1 : (direction == 'L' ? -1 : 0);
+    var y = direction == 'U' ? 1 : (direction == 'D' ? -1 : 0);
+    return Point(x, y);
+  }
+
+  void updateRope(Point<int> dir) {
+    knots[0] += dir;
+
+    for (int i = 1; i < knots.length; i++) {
+      var head = knots[i - 1];
+      var tail = knots[i];
+
+      if ((head.x - tail.x).abs() > 1 || (head.y - tail.y).abs() > 1) {
+        var xDir =
+            head.x == tail.x ? 0 : (head.x - tail.x) ~/ (head.x - tail.x).abs();
+        var yDir =
+            head.y == tail.y ? 0 : (head.y - tail.y) ~/ (head.y - tail.y).abs();
+        knots[i] += Point(xDir, yDir);
+      }
+    }
+  }
+
+  input.forEach((line) {
+    var instruction = line.split(' ');
+    var dirPoint = getDirectionPoint(instruction.first);
+    var steps = int.parse(instruction.last);
+
+    for (int i = 0; i < steps; i++) {
+      updateRope(dirPoint);
+      tailVisited.add(Point(knots.last.x, knots.last.y));
+    }
+  });
+
+  print('part 02 : ${tailVisited.length}');
 }
