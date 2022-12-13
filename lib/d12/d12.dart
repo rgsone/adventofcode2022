a10338f6b94ce027cf4443ad45f7175cb76b963c
rgsone <rgs@rgsone.com>
2022-12-13 15:12:13 +0100

    refactor d12

--- lib/d12/d12.dart
+++ lib/d12/d12.dart
@@ -13,13 +13,28 @@ void run() {
 ////////////////////////////////////////////////////////////////////
 
 void resolvePart1(List<String> input) {
-  var result = 0;
+  print('part 01 : ${solve(input, true)}');
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+void resolvePart2(List<String> input) {
+  print('part 02 : ${solve(input, false)}');
+}
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+int solve(List<String> input, bool isPart1) {
+  var letterStart = isPart1 ? 'S' : 'E';
+  var letterEnd = isPart1 ? 'E' : 'a';
   var grid = <List<String>>[];
   Position start = Position(0, 0, 0);
 
   for (int i = 0; i < input.length; i++) {
     var cols = input[i].split('');
-    var startCol = cols.indexWhere((e) => e == 'S');
+    var startCol = cols.indexWhere((e) => e == letterStart);
     if (startCol > -1) start = Position(i, startCol, start.steps);
     grid.add(cols);
   }
@@ -38,7 +53,6 @@ void resolvePart1(List<String> input) {
   queue.add(start);
   visited.add(start);
 
-  outer:
   while (queue.isNotEmpty) {
     var cur = queue.removeAt(0);
 
@@ -48,85 +62,30 @@ void resolvePart1(List<String> input) {
 
       if (nRow < 0 || nRow >= rows || nCol < 0 || nCol >= cols) continue;
 
-      if ((getHeight(grid[nRow][nCol]) - getHeight(grid[cur.row][cur.col])) > 1)
+      if ((isPart1 &&
+              getHeight(grid[nRow][nCol]) - getHeight(grid[cur.row][cur.col]) >
+                  1) ||
+          (!isPart1 &&
+              getHeight(grid[nRow][nCol]) - getHeight(grid[cur.row][cur.col]) <
+                  -1)) {
         continue;
+      }
 
       if (visited.any((e) => e.row == nRow && e.col == nCol)) continue;
 
       var n = Position(nRow, nCol, cur.steps + 1);
       visited.add(n);
 
-      if (grid[n.row][n.col] == 'E') {
-        result = n.steps;
-        break outer;
+      if (grid[n.row][n.col] == letterEnd) {
+        return n.steps;
       }
 
       queue.add(n);
     }
   }
 
-  print('part 01 : $result');
+  return 0;
 }
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
-
-void resolvePart2(List<String> input) {
-  var result = 0;
-  var grid = <List<String>>[];
-  Position start = Position(0, 0, 0);
-
-  for (int i = 0; i < input.length; i++) {
-    var cols = input[i].split('');
-    var startCol = cols.indexWhere((e) => e == 'E');
-    if (startCol > -1) start = Position(i, startCol, start.steps);
-    grid.add(cols);
-  }
-
-  var rows = grid.length;
-  var cols = grid.first.length;
-  var queue = <Position>[];
-  var visited = <Position>{};
-  var directions = <List<int>>[
-    [-1, 0],
-    [0, -1],
-    [1, 0],
-    [0, 1]
-  ];
-
-  queue.add(start);
-  visited.add(start);
-
-  outer:
-  while (queue.isNotEmpty) {
-    var cur = queue.removeAt(0);
-
-    for (var dir in directions) {
-      var nRow = cur.row + dir[0];
-      var nCol = cur.col + dir[1];
-
-      if (nRow < 0 || nRow >= rows || nCol < 0 || nCol >= cols) continue;
-      if ((getHeight(grid[nRow][nCol]) - getHeight(grid[cur.row][cur.col])) <
-          -1) continue;
-      if (visited.any((e) => e.row == nRow && e.col == nCol)) continue;
-
-      var n = Position(nRow, nCol, cur.steps + 1);
-      visited.add(n);
-
-      if (grid[n.row][n.col] == 'a') {
-        result = n.steps;
-        break outer;
-      }
-
-      queue.add(n);
-    }
-  }
-
-  print('part 02 : $result');
-}
-
-////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////
 
 int getHeight(String letter) {
   return 'SabcdefghijklmnopqrstuvwxyzE'.indexOf(letter);
